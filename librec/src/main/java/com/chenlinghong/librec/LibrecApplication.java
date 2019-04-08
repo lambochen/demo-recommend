094749cb4d9443afe50d4420d84e0f8afec3e10f
chenlinghong <chenlinghong@126.com>
2019-04-08 08:57:16 +0800

    添加librec module

---
+++ librec/src/main/java/com/chenlinghong/librec/LibrecApplication.java
@@ -0,0 +1,13 @@
+package com.chenlinghong.librec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrecApplication.class, args);
+    }
+
+}
