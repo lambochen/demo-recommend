d697a511d32e516aca81be05cbdafe0242b3d85f
chenlinghong <chenlinghong@126.com>
2019-04-23 08:41:07 +0800

    mahout: 添加数据集intro.csv

---
+++ mahout/src/main/java/com/chenlinghong/mahout/UserBasedRecommender.java
@@ -0,0 +1,18 @@
+package com.chenlinghong.mahout;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+
+/**
+ * @Description 基于用户推荐
+ * @Author chenlinghong
+ * @Date 2019/4/21 12:14
+ * @Version V1.0
+ */
+public class UserBasedRecommender {
+
+    public void userBasedRecommender() throws TasteException {
+
+
+    }
+
+}
