4096585aa3b67f5bf6e99d0a7cc6495b9bc206cd
chenlinghong <chenlinghong@126.com>
2019-04-23 13:42:36 +0800

    mahout：基于物品推荐，添加评分

--- mahout/src/main/java/com/chenlinghong/mahout/ItemBasedRecommender.java
+++ mahout/src/main/java/com/chenlinghong/mahout/ItemBasedRecommender.java
@@ -1,7 +1,12 @@
 package com.chenlinghong.mahout;
 
 import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.IRStatistics;
 import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.eval.RecommenderIRStatsEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.GenericRecommenderIRStatsEvaluator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
@@ -25,7 +30,7 @@ public class ItemBasedRecommender {
 
     @Test
     public void itemBasedRecommender() throws IOException, TasteException {
-        File modelFile = new File("F:\\code\\demo-recommend\\mahout\\data\\cf\\intro.csv");
+        File modelFile = new File("F:\\code\\demo-recommend\\mahout\\data\\cf\\ml-ratings.dat");
 
         // 数据模型
         DataModel dataModel = new FileDataModel(modelFile);
@@ -40,7 +45,7 @@ public class ItemBasedRecommender {
         };
 
         // 获取推荐结果,给用户1推荐4条数据
-        List<RecommendedItem> itemList = recommenderBuilder.buildRecommender(dataModel).recommend(1,4);
+        List<RecommendedItem> itemList = recommenderBuilder.buildRecommender(dataModel).recommend(1, 4);
 
         /**
          * 输出推荐结果
@@ -50,9 +55,24 @@ public class ItemBasedRecommender {
         }
 
         /**
-         *
+         * 计算评分
          */
+        RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
 
+        // 使用70%的数据训练，另外30%的数据测试
+        double score = evaluator.evaluate(recommenderBuilder, null, dataModel, 0.7, 1.0);
+        System.out.println("evaluator#evalute: score = " + score);
+
+        // 计算查全率和查准率
+        RecommenderIRStatsEvaluator statsEvaluator = new GenericRecommenderIRStatsEvaluator();
+
+        // Evaluate precision and recall "at 2":
+        IRStatistics stats = statsEvaluator.evaluate(recommenderBuilder,
+                null, dataModel, null, 4,
+                GenericRecommenderIRStatsEvaluator.CHOOSE_THRESHOLD,
+                1.0);
+        System.out.println("precision = " + stats.getPrecision());
+        System.out.println("recall = " + stats.getRecall());
 
     }
 }
